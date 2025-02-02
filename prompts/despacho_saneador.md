3324b44ebf666cdf40369967fde3d7c049d1abe4
Alan <alanmesquita2022@gmail.com>
2025-02-01 23:10:44 -0300

    estrutura base

---
+++ prompts/despacho_saneador.md
@@ -0,0 +1,9 @@
+# Tarefa
+Analise o texto fornecido e determine se ele corresponde a um despacho saneador ou a outro tipo de documento jurídico. O despacho saneador é um documento jurídico no qual o juiz constata a existência de elementos mínimos para que a fase probatória do processo possa ser iniciada, relatando o que já foi apresentado, corrigindo eventuais defeitos e indicando os próximos passos. Os elementos mínimos para o prosseguimento do processo verificados no despacho saneador são: se os atos até então praticados no processo são válidos e se há alguma questão processual pendente. Em seguida, o despacho saneador definirá quais são as questões jurídicas relevante para o julgamento; se há fatos incontroversos, que não mais precisarão ser discutidos, e quais são os fatos controversos, que precisarão, iniciada a fase probatória, ser provados pelas partes; quais serão as provas admitidas, se periciais e/ou testemunhais, e que deverão ser produzidas na fase de instrução; sobre quem recairá o dever de provar cada um dos fatos controversos; e, ao final, convocará um perito e/ou designará uma audiência de instrução. No despacho saneador, o juiz também poderá anunciar que julgará o processo antecipadamente, se chegar à conclusão de que as alegações das partes já estão provadas do modo como estão e são suficientes para uma decisão de mérito. Baseie sua resposta na estrutura, no conteúdo e no propósito do documento analisado, conforme as instruções.
+
+# Formato de Saida
+Responda Despacho Saneador ou Outros em Rótulo
+
+# Prediction
+Texto: {{ Texto }}
+Rótulo:
---
+++ tasks.py
@@ -0,0 +1,89 @@
+import requests
+import json
+import concurrent.futures
+from abc import ABC, abstractmethod
+from typing import List, Dict, Callable
+from tqdm import tqdm
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, classification_report
+
+class DataProcessor(ABC):
+    def __init__(self, data_dir, max_threads=1):
+        self.data_dir = data_dir
+        self.max_threads = max_threads
+
+    @abstractmethod
+    def get_train_examples(self):
+        pass
+
+    @abstractmethod
+    def get_test_examples(self):
+        pass
+
+    @abstractmethod
+    def evaluate(self, predictor, test_exs):
+        pass
+
+    @abstractmethod
+    def stringify_prediction(self, pred):
+        pass
+
+
+
+
+def process_example(ex, predictor, prompt):
+    pred = predictor.inference(ex, prompt)
+    return ex, pred
+
+
+class ClassificationTask(DataProcessor):
+
+    def run_evaluate(self, predictor, prompt, test_exs, n=100):
+        labels = []
+        preds = []
+        texts = []
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.max_threads) as executor:
+            futures = [executor.submit(process_example, ex, predictor, prompt) for ex in test_exs[:n]]
+            for i, future in tqdm(enumerate(concurrent.futures.as_completed(futures)), total=len(futures), desc='running evaluate'):
+                ex, pred = future.result()
+                texts.append(ex['text'])
+                labels.append(ex['label'])
+                preds.append(pred)
+
+        accuracy = accuracy_score(labels, preds)
+        f1 = f1_score(labels, preds, average='micro')
+        return f1, texts, labels, preds
+
+    def evaluate(self, predictor, prompt, test_exs, n=100):
+        while True:
+            try:
+                f1, texts, labels, preds = self.run_evaluate(predictor, prompt, test_exs, n=n)
+                break
+            except (concurrent.futures.process.BrokenProcessPool, requests.exceptions.SSLError):
+                pass
+        return f1, texts, labels, preds
+
+
+class BinaryClassificationTask(ClassificationTask):
+    categories = ['No', 'Yes']
+
+    def stringify_prediction(self, pred):
+        return BinaryClassificationTask.categories[pred]
+
+
+class DefaultHFBinaryTask(BinaryClassificationTask):
+    categories = ['No', 'Yes']
+
+    def get_train_examples(self):
+        exs = []
+        for i, row in enumerate(open(self.data_dir + '/training_data.jsonl', encoding="utf-8")):
+            row = json.loads(row.strip())
+            exs.append({'id': f'train-{i}', 'label': row['label'], 'text': row['text']})
+        return exs
+    
+    def get_test_examples(self):
+        exs = []
+        for i, row in enumerate(open(self.data_dir + '/test_data.jsonl', encoding="utf-8")):
+            row = json.loads(row.strip())
+            exs.append({'id': f'test-{i}', 'label': row['label'], 'text': row['text']})
+        return exs
