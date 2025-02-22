54a2288eb40b0bc003a9f5f56caa3087aea2d390
Alan <alanmesquita2022@gmail.com>
2025-02-22 13:44:55 -0300

    fix

--- optimizers.py
+++ optimizers.py
@@ -202,7 +202,7 @@ class ProTeGi(PromptOptimizer):
             task_section = sections['tarefa'].strip()
 
             # evaluate prompt on minibatch
-            _, texts, labels, preds = task.evaluate(gpt4, prompt, minibatch)
+            _, _, _, _, texts, labels, preds = task.evaluate(gpt4, prompt, minibatch)
 
             # get gradients
             new_task_sections = []
